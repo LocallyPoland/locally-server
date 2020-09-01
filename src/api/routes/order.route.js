d3e9d8d0694bfc2d98d236663dd1e06c31f8718e
vadvit009 <vadvit009@gmail.com>
2020-09-01 15:15:53 +0300

    added some new docs

--- src/api/routes/order.route.js
+++ src/api/routes/order.route.js
@@ -27,12 +27,214 @@ app.get("/order", verifyAdminToken, get.getAllOrders);
  */
 app.get("/order/stats", get.orderStats);
 
+/**
+ * @swagger
+ *  /api/v1/order/:id:
+ *  get:
+ *    tags:
+ *    - Orders Routes
+ *    parameters:
+ *       - in: req.headers
+ *         name: Authorization
+ *         description: ADMIN_TOKEN
+ *         type: string
+ *         required: true
+ *    responses:
+ *      '200':
+ *         description: Success
+ *      '400':
+ *        description: Bad Request
+ */
 app.get("/order/:id", verifyAdminToken, get.getOrder);
 
+/**
+ * @swagger
+ *  /api/v1/order:
+ *  post:
+ *    tags:
+ *    - Orders Routes
+ *    parameters:
+ *       - in: req.headers
+ *         name: Authorization
+ *         description: USER_TOKEN
+ *         type: string
+ *         required: true
+ *       - in: req.body
+ *         name: userID
+ *         description: User's ID.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: parcel
+ *         description: parcel Array.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: sum
+ *         description: Order sum.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: weight
+ *         description: parcel weight.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: length
+ *         description: parcel length.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: status
+ *         description: Order status.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: pickUp
+ *         description: parcel pickUp.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryCity
+ *         description: parcel deliveryCity.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryStreet
+ *         description: parcel deliveryStreet.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryHouse
+ *         description: parcel deliveryHouse.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryApartament
+ *         description: parcel deliveryApartament.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: paymentType
+ *         description: order paymentType.
+ *         required: true
+ *         type: string
+ *    responses:
+ *      '200':
+ *         description: Success
+ *      '400':
+ *        description: Bad Request
+ */
 app.post("/order", verifyUserToken, post.createOrder);
 
+/**
+ * @swagger
+ *  /api/v1/order/:id:
+ *  patch:
+ *    tags:
+ *    - Orders Routes
+ *    parameters:
+ *       - in: req.headers
+ *         name: Authorization
+ *         description: ADMIN_TOKEN
+ *         type: string
+ *         required: true
+ *       - in: req.params
+ *         name: id
+ *         description: id for found such order and patch.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: userID
+ *         description: User's ID.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: parcel
+ *         description: parcel Array.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: sum
+ *         description: Order sum.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: weight
+ *         description: parcel weight.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: length
+ *         description: parcel length.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: status
+ *         description: Order status.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: pickUp
+ *         description: parcel pickUp.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryCity
+ *         description: parcel deliveryCity.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryStreet
+ *         description: parcel deliveryStreet.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryHouse
+ *         description: parcel deliveryHouse.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: deliveryApartament
+ *         description: parcel deliveryApartament.
+ *         required: true
+ *         type: string
+ *       - in: req.body
+ *         name: paymentType
+ *         description: order paymentType.
+ *         required: true
+ *         type: string
+ *    responses:
+ *      '200':
+ *         description: Success
+ *      '400':
+ *        description: Bad Request
+ */
 app.patch("/order/:id", verifyAdminToken, patch.updateOrder);
 
+/**
+ * @swagger
+ *  /api/v1/order/:id:
+ *  delete:
+ *    tags:
+ *    - Orders Routes
+ *    parameters:
+ *       - in: req.headers
+ *         name: Authorization
+ *         description: ADMIN_TOKEN
+ *         type: string
+ *         required: true
+ *       - in: req.params
+ *         name: id
+ *         description: id for found such order and delete.
+ *         required: true
+ *         type: string
+ *    responses:
+ *      '200':
+ *         description: Deleted
+ *      '400':
+ *        description: Bad Request
+ */
 app.delete("/order/:id", verifyAdminToken, delte.deleteOrder);
 
 // app.patch("/order/restore/:id", verifyAdminToken, patch.restoreUser);
