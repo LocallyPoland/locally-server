cd32c8f86568e694087cb508d2db7677a3d99f26
vadvit009 <vadvit009@gmail.com>
2020-09-08 15:11:37 +0300

    added admin build

---
+++ build/precache-manifest.0b03091a3e3cf3b426e53210d0b2f663.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835e42f9d35fb65549cf0a546b900671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "401d4f6decf7e0c3cf99",
+    "url": "/static/css/2.964ba072.chunk.css"
+  },
+  {
+    "revision": "93bb8475b2cde34b5ddf",
+    "url": "/static/css/main.37f3a380.chunk.css"
+  },
+  {
+    "revision": "401d4f6decf7e0c3cf99",
+    "url": "/static/js/2.4b25a0d5.chunk.js"
+  },
+  {
+    "revision": "26276ad57cd197a7db42445302e8a7d6",
+    "url": "/static/js/2.4b25a0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93bb8475b2cde34b5ddf",
+    "url": "/static/js/main.08c0c8a3.chunk.js"
+  },
+  {
+    "revision": "e01e7c58a1225dd65711",
+    "url": "/static/js/runtime-main.9055d155.js"
+  },
+  {
+    "revision": "329390e39120aacb239473e56df0de4d",
+    "url": "/static/media/arrow-forward.329390e3.svg"
+  },
+  {
+    "revision": "92d7eb186b1af7a1df02534e4d9690de",
+    "url": "/static/media/close.92d7eb18.svg"
+  },
+  {
+    "revision": "623c44144b182d80b2b60e460dccedb0",
+    "url": "/static/media/connector.623c4414.svg"
+  },
+  {
+    "revision": "7d7c0ba2e6cdbb43e6fdc5d8aa10f8f5",
+    "url": "/static/media/logout.7d7c0ba2.svg"
+  },
+  {
+    "revision": "c514c2847f97726d5b58f986bb4a7a59",
+    "url": "/static/media/symbol.c514c284.png"
+  }
+]);
