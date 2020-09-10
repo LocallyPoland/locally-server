56a618ad62d3bd163d6e805f20cd21564a81d909
vadvit009 <vadvit009@gmail.com>
2020-09-10 15:21:25 +0300

    get order update & order with user

--- src/api/contollers/order/get/get.controller.js
+++ src/api/contollers/order/get/get.controller.js
@@ -25,7 +25,9 @@ const getAllOrders = async (req, res) => {
 
 const getOrder = async (req, res) => {
     const {id} = req.params;
-    return await Order.findById(id).exec((err, order) => {
+    return await Order.findById(id).populate({
+        path: "userID",
+    }).exec((err, order) => {
         if (err) return res.send(err);
         res.send(order);
     });
