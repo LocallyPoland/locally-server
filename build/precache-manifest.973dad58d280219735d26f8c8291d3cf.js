9bbaeac1558db6133e6da0d7481a5a4b55e07e82
vadvit009 <vadvit009@gmail.com>
2020-09-10 15:28:26 +0300

    new build

---
+++ build/precache-manifest.973dad58d280219735d26f8c8291d3cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5ab0d95c7cabf6fe81715aa6b8a4b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b4c5f7150e38ca3f7d9",
+    "url": "/static/css/2.964ba072.chunk.css"
+  },
+  {
+    "revision": "ebb40045f7a7fdf98b0c",
+    "url": "/static/css/main.ec411a6a.chunk.css"
+  },
+  {
+    "revision": "5b4c5f7150e38ca3f7d9",
+    "url": "/static/js/2.9439c301.chunk.js"
+  },
+  {
+    "revision": "26276ad57cd197a7db42445302e8a7d6",
+    "url": "/static/js/2.9439c301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb40045f7a7fdf98b0c",
+    "url": "/static/js/main.b7b0997a.chunk.js"
+  },
+  {
+    "revision": "e01e7c58a1225dd65711",
+    "url": "/static/js/runtime-main.9055d155.js"
+  },
+  {
+    "revision": "11811faea5dbc41573a434624f3ff09e",
+    "url": "/static/media/arrow-forward.11811fae.svg"
+  },
+  {
+    "revision": "c02fb23ea70697dbdbeadd6667249515",
+    "url": "/static/media/close.c02fb23e.svg"
+  },
+  {
+    "revision": "9e30d28a2aaaa3c33f234ad787077e9c",
+    "url": "/static/media/connector.9e30d28a.svg"
+  },
+  {
+    "revision": "7d7c0ba2e6cdbb43e6fdc5d8aa10f8f5",
+    "url": "/static/media/logout.7d7c0ba2.svg"
+  },
+  {
+    "revision": "c514c2847f97726d5b58f986bb4a7a59",
+    "url": "/static/media/symbol.c514c284.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fde58d160881650193fa174c51c15a1c.js"
+  "/precache-manifest.973dad58d280219735d26f8c8291d3cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
