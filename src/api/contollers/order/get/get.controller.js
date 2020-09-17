6069f0f0c40fe04d7419060277614f5c455c0f66
vadvit009 <vadvit009@gmail.com>
2020-09-17 13:02:41 +0300

    update func sortbydate

--- src/api/contollers/order/get/get.controller.js
+++ src/api/contollers/order/get/get.controller.js
@@ -179,7 +179,7 @@ const getOrdersHistory = async (req, res) => {
     const {from, to, status, sort, search} = req.query;
 
     const sortByDate = () => {
-        if (from === undefined && to === undefined) {
+        if ((from === undefined && to === undefined) ||( from === "undefined"&& to === "undefined")) {
             return {
                 createdAt: {$gte: new Date(0), $lte: new Date()}
             }
