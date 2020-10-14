a9ef9a570c74105d36f8c532b7b313e25e878719
vadvit009 <vadvit009@gmail.com>
2020-10-14 17:08:39 +0300

    new timechecker'

--- src/api/utils/timeCheck.js
+++ src/api/utils/timeCheck.js
@@ -5,7 +5,7 @@ const timeChecker = (req, res, next) => {
         .then(variable => {
             const time = new Date;
             // if (variable.timeStart === time.getHours() && variable.timeStop === time.getHours() && !variable.switcher) {
-                if (variable.timeStart <= time.getHours() && variable.timeStop >= time.getHours() && !variable.switcher) {
+            if (variable.timeStart >= time.getHours() && variable.timeStop <= time.getHours() && !variable.switcher) {
                 res.send({error: true})
             } else {
                 next();
