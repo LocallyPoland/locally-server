7f5d9c5d404545553c1256579f1e08b034d2a24d
vadvit009 <vadvit009@gmail.com>
2020-10-14 14:37:12 +0300

    new api keu sendgrid

--- src/api/utils/timeCheck.js
+++ src/api/utils/timeCheck.js
@@ -1,17 +1,18 @@
-const { Variables } = require('../models');
+const {Variables} = require('../models');
 
 const timeChecker = (req, res, next) => {
-  return Variables.findById("5f4781753847c91fad9fd141")
-    .then(variable => {
-      const time = new Date;
-      if (variable.timeStart === time.getHours() && variable.timeStop === time.getHours() && !variable.switcher) {
-        res.send({ error: true })
-      } else {
-        next();
-      }
-    }).catch(err => console.error(err))
+    return Variables.findById("5f4781753847c91fad9fd141")
+        .then(variable => {
+            const time = new Date;
+            console.log(variable)
+            if (variable.timeStart === time.getHours() && variable.timeStop === time.getHours() && !variable.switcher) {
+                res.send({error: true})
+            } else {
+                next();
+            }
+        }).catch(err => console.error(err))
 }
 
 module.exports = {
-  timeChecker
+    timeChecker
 };
