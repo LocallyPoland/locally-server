3da098e0976fd67189a354496f045a642761f31b
vadvit009 <vadvit009@gmail.com>
2020-10-14 17:20:36 +0300

    delete order fix

--- src/api/contollers/order/delete/delete.controller.js
+++ src/api/contollers/order/delete/delete.controller.js
@@ -2,7 +2,7 @@ const { Order } = require("../../../models");
 
 const deleteOrder = async (req, res) => {
   const { id } = req.params;
-  return await Order.findByIdAndDelete(id).exec((err, order) => {
+  return await Order.findByIdAndRemove(id).exec((err, order) => {
     if (err) return res.send(err);
     res.status(200).send("Deleted");
   });
